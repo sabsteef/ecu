72b20b4c186971b9b3de6d9098d7863e32870eeb
sabsteef <38842670+sabsteef@users.noreply.github.com>
2019-08-04 01:06:20 +0200

    Update base.py

--- eculib/base.py
+++ eculib/base.py
@@ -1,32 +1,56 @@
 import os, sys, platform
-from pyftdi.ftdi import Ftdi
-import usb.core
+from pylibftdi import Device
 from ctypes import *
 import time
 
-class KlineAdapter(Ftdi):
+class KlineAdapter(Device):
 
-	def __init__(self, device, baudrate=10400):
-		super(KlineAdapter, self).__init__()
-		self.open_from_device(device)
-		self.set_baudrate(baudrate)
-		self.set_line_property(8, 1, "N")
+	def __init__(self, device_id, baudrate=10400):
+		super(KlineAdapter, self).__init__(device_id, auto_detach=(platform.system()!="Windows"))
+		self.baudrate = baudrate
+		self.ftdi_fn.ftdi_usb_reset()
+		self.ftdi_fn.ftdi_set_line_property(8, 1, 0)
+		self.ftdi_fn.ftdi_usb_purge_buffers()
 
 	def kline(self):
-		ret = False
-		self.purge_buffers()
-		starttime = time.time()
-		while True:
-			self.write_data(b"\xff")
+		self.flush()
+		self._write(b"\xff")
+		for _ in range(20):
 			time.sleep(.002)
-			tmp = self._read()
-			if len(tmp) == 3:
-				ret = (tmp[2] == 0xff)
-				break
-			if time.time() - starttime > 1:
-				break
-		self.purge_buffers()
-		return ret
+			b = self._read(1)
+			if len(b) > 0 and b == b"\xff":
+				return True
+		return False
+
+	def KWP_SlowInit(self):
+		self.ftdi_fn.ftdi_set_bitmode(1, 0x00)
+		self._write(b'\x00')
+		time.sleep(.2)
+		self._write(b'\x01')
+		time.sleep(.4)
+		self._write(b'\x00')
+		time.sleep(.4)
+		self._write(b'\x01')
+		time.sleep(.4)
+		self._write(b'\x00')
+		time.sleep(.4)
+		self._write(b'\x01')
+		time.sleep(.2)
+		self.ftdi_fn.ftdi_set_bitmode(0, 0x00)
+		self.ftdi_fn.ftdi_usb_purge_buffers()
+
+	def KWP_FastInit(self):
+		self.ftdi_fn.ftdi_set_bitmode(1, 0x00)
+		self._write(b'\x00')
+		time.sleep(.025)
+		self._write(b'\x01')
+		time.sleep(.025)
+		self.ftdi_fn.ftdi_set_bitmode(0, 0x00)
+		self.ftdi_fn.ftdi_usb_purge_buffers()
+		self._write(b"\xc1\x33\xf1\x81\x66")
+		self._read(5)
+		self._read(7)
+		self.ftdi_fn.ftdi_usb_purge_buffers()
 
 class ECU(object):
 
